c46e9de655bf72c84ab6a9a54cf3f86cd2eef173
Nimbleansah <nimbleansahyeboah@gmail.com>
2023-06-16 11:52:23 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; aa <= 'z' ; a++)
+	{
+		putchar(a);
+
+	}
+
+	for (a = 'A' ; a <= 'Z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
