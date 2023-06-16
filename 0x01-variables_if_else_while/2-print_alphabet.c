02ebb7a2b6d36736499110773b4ec4079596c4a0
Nimbleansah <nimbleansahyeboah@gmail.com>
2023-06-16 11:36:48 +0000

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ *
+ *main entry point
+ * 
+ * Return:Always (0) Success
+ *
+ */
+int main(void)
+{
+	char a;
+	for (a = "a"; a<="z";a++)
+	{
+		putcher(a);
+	}
+	putcher("\n");
+	return (0);
+}
