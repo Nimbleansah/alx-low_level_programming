c9746e7fa4b9e0dce8f9e885d6f9c121688c6613
Nimbleansah <nimbleansahyeboah@gmail.com>
2023-06-16 12:29:21 +0000

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z' ; z >= 'a' ; z--)
+	{
+		putchar(z);
+		putchar('\n');
+	}
+	return (0);
+}
+
