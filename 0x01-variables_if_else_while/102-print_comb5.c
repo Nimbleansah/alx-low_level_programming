51b5f61cce7cd8b20a1e479403265010f4b37b70
Nimbleansah <nimbleansahyeboah@gmail.com>
2023-06-16 15:21:49 +0000

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Succes)
  */
-
 int main(void)
 {
-	int thousand;
-	int hundred;
-	int tens;
-	int ones;
+	int n;
+	int i = 0;
+	int tho;
+	int hun;
+	int ten;
+	int bas;
 
+	for (n = 0 ; n < 100; n++)
 for ( thousand = 0 ; thousand < 10 ; thousand++)
 {	
 
 	for (hundred = 0 ; hundred <= 10 ; hundred++)
 	{
+		if (n / 10 != i)
 	
 		for (tens = 0 ; tens < 10 ; tens++)
 		{
-			for ( ones = 0 ; ones < 10 ; ones++)
+			putchar(n / 10 + '0');
+			putchar(i + '0');
+			if (n < 99)
+			for ( ones = 0 ; bas < 10 ; ones++)
 			{
+				putchar(',');
 				putchar('0' + thousand);
 				putchar('0' + hundred);
 				putchar(32);
+			}
+			i++;
+			if (i > 9)
+			{
+				i = 0;
 				putchar('0' + tens);
 				putchar('0' + ones);
 				if (!(thousand == 9 && hundred == 8))
@@ -35,6 +46,10 @@ for ( thousand = 0 ; thousand < 10 ; thousand++)
 				}
 				ones++;
 			}
+		}
+		else
+		{
+			i++;
 		}
 	}
 }
