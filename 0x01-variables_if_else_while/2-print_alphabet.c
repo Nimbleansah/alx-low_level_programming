65e73082f25b95cfcb926ea726175621f7c6c897
Nimbleansah <nimbleansahyeboah@gmail.com>
2023-06-20 08:35:37 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /*
- *
  *main entry point
- * 
  * Return:Always (0) Success
- *
  */
 int main(void)
 {
 	int count;
 	char a[26] = "abcdefghijklmnopqrstuvwxyz";
-	for (count = 0; count < 26 ; count++)
+	for(count =0; count < 26 ; count++)
 	{
 		putchar(a[count]);
 	}
