1445d6816b79c04e0babeaf655447a85063b3d40
Nimbleansah <nimbleansahyeboah@gmail.com>
2023-06-20 11:50:49 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+/**
+ * print_sign - prints the sign of the given number +/-
+ * on: Number to be tested
+ *
+ *
+ */
+int print_sign(int a)
+{
+	if (a > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (a < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
