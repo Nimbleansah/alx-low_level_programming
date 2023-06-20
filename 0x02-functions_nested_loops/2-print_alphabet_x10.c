cd1c073b451157774e8d4b5b752efc9c7aa4389b
Nimbleansah <nimbleansahyeboah@gmail.com>
2023-06-20 11:38:13 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <std.h>
+/**
+ * print_alphabet_x10 - write the the lower-case alphabet on a line.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (a = 'a' ; a <= 'z' ; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
+
+
